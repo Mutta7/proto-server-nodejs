ee2a696b31464c71289214907305c625e0a3be19
Nozomu Hasegawa <hsnznewton7@gmail.com>
2022-03-28 23:21:54 +0900

    compose asset folders & implement router

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
-const routeResponseMap ={
-    "/": "views/index.html",
-}
-
-const getViewUrl = url => {
-    return `views${url}.html`;
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.NOT_FOUND, {
+        "Content-Type": "text/html"
+    });
+    res.write("<h1>File Not Found!</h1>");
+    res.end();
 }
 
 const port=3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
-    fs = require("fs");
+    router = require("./router"),
+    fs = require("fs"),
+    plainTextContentType ={
+        "Content-Type": "text/plain"
+    },
+    htmlContentType ={
+        "Content-Type": "text/html"
+    };
 
-http.createServer((req,res) => {
-    let viewUrl = getViewUrl(req.url);
-
-    fs.readFile(viewUrl, (error, data) => {
-        if(error) {
-            res.writeHead(httpStatus.NOT_FOUND);
-            res.write("<h1>FILE NOT FOUND</h1>")
-        } else {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/html"
-            });
-            res.write(data);
+const customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+        if(errors){
+            console.log("Error reading the file...");
         }
-        res.end();
-    });
+        res.end(data);
+    })
+}
 
-    
-}).listen(port);
+router.get("/", (req, res)=>{
+    res.writeHead(httpStatus.OK,plainTextContentType);
+    res.end("INDEX");
+});
+
+router.get("/index.html", (req, res)=>{
+    res.writeHead(httpStatus.OK,htmlContentType);
+    customReadFile("views/index.html", res);
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatus.OK, plainTextContentType);
+    res.end("POSTED");
+    console.log("posted!!");
+})
+
+http.createServer(router.handle).listen(port);
 
 console.log(`The Server has started and is litening on port number: ${port}`);
+
+
