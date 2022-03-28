9fe2c46bfce12a9ab8c19a867f9f90a4bc60f8c8
Nozomu Hasegawa <hsnznewton7@gmail.com>
2022-03-28 16:34:11 +0900

    add route

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const routeResponseMap ={
+    "/info": "<h1>Info Page</h1>",
+    "/contact": "<h1>Contact Us</h1><div>Say hello by emailing <a href='mailto: sample@contoso.com'>here</a></div>",
+    "/aboutUs": "<h1>Learn more about us</h1>",
+    "/error": "Sorry, the page you are looking for is not here.",
+}
+
 const port=3000,
     http = require("http"),
     httpStatus = require("http-status-codes"),
@@ -6,26 +13,25 @@ const getJsonString = obj => {
     return JSON.stringify(obj,null,2);
 }
 
+let defaultResponseMessage = `<h1>This will show on the screen.</h1>`
+
 app.on("request", (req,res) =>{
-    var body = [];
-    req.on("data", (bodyData) => {
-        body.push(bodyData);
-    });
-    req.on("end", ()=> {
-        body = Buffer.concat(body).toString();
-        console.log(`Request Body Contents: ${body}`);
-    });
-
-    console.log(`Method : ${getJsonString(req.method)}`);
-    console.log(`URL : ${getJsonString(req.url)}`);
-    console.log(`Headers : ${getJsonString(req.headers)}`);
-
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    });
-
-    let responseMessage = `<h1>This will show on the screen.</h1>`
-    res.end(responseMessage);
+    if(req.url =="/error"){
+        res.writeHead(404, {
+            "Content-Type": "text/plain"
+        });
+        res.end(routeResponseMap[req.url]);
+    } else if(routeResponseMap[req.url]){
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.end(routeResponseMap[req.url]);
+    } else {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.end(defaultResponseMessage)
+    }
 });
 
 app.listen(port);
